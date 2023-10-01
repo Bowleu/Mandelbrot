26d02e6bcc76d0c710b6c3d598d41a0ea5589270
Arsenie <Arsenie2004@gmail.com>
2023-10-01 13:28:03 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,185 @@
+﻿#include <iostream>
+#include <glad.h>
+#include <glfw3.h>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <time.h>
+#include <Windows.h>
+
+using namespace std;
+
+int windowSizeY = 1080;
+int windowSizeX = 1920;
+
+const char* loadFile(const char* fileName) {
+    FILE* shader;
+    char* chars = new char[8000];
+    memset(chars, 0, 8000);
+    fopen_s(&shader, fileName, "rb");
+    fread(chars, sizeof(char), 8000, shader);
+    fclose(shader);
+    return chars;
+}
+
+const char* vertex_shader = loadFile("vertexShader.glsl");
+const char* fragment_shader = loadFile("fragmentShader.glsl");;
+
+void _init_() {
+    gladLoadGL();
+}
+
+void glfwWindowSizeCallback(GLFWwindow* window, int width, int height) {
+    windowSizeX = width;
+    windowSizeY = height;
+    glViewport(0, 0, windowSizeX, windowSizeY);
+}
+
+void glfwKeyCallback(GLFWwindow* window, int key, int scanCode, int action, int node) {
+    if (key == GLFW_KEY_ESCAPE and action == GLFW_PRESS ) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
+
+GLfloat points[12] = {-1.0,  1.0, 0.0, 
+                      -1.0, -1.0, 0.0,
+                       1.0, -1.0, 0.0,
+                       1.0,  1.0, 0.0};
+
+
+GLuint createShaderProgram() {
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(vertexShader, 1, &vertex_shader, nullptr);
+    glShaderSource(fragmentShader, 1, &fragment_shader, nullptr);
+
+    int success;
+    GLchar inf[500];
+
+    glCompileShader(vertexShader);
+
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        cout << "Vertex shader problem!\n";
+        glGetShaderInfoLog(vertexShader, 500, NULL, inf);
+        for (int i = 0; i < 500; i++)
+            cout << inf[i];
+        cout << '\n';
+    }
+
+    glCompileShader(fragmentShader);
+    success = 1;
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        cout << "Fragment shader problem!\n";
+        glGetShaderInfoLog(fragmentShader, 500, NULL, inf);
+        for (int i = 0; i < 500; i++)
+            cout << inf[i];
+        cout << '\n';
+    }
+
+    GLuint sp = glCreateProgram();
+    glAttachShader(sp, vertexShader);
+    glAttachShader(sp, fragmentShader);
+    glLinkProgram(sp);
+
+    success = 1;
+    glGetProgramiv(sp, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(sp, 500, NULL, &inf[0]);
+        cout << "Bad linkin'!\n";
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    return sp;
+}
+
+int main(void)
+{
+    setlocale(0, "");
+    /* Initialize the library */
+    if (!glfwInit())
+    {
+        cout << "GLFW initialisation failed!\n";
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+
+    /* Create a windowed mode window and its OpenGL context */
+    GLFWwindow* window = glfwCreateWindow(1080, 1080, "Hello World", NULL, NULL);
+
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    _init_();
+
+    cout << "OpenGL version: " << glGetString(GL_VERSION) << '\n';
+    glfwSetWindowSizeCallback(window, glfwWindowSizeCallback);
+    glfwSetKeyCallback(window, glfwKeyCallback);
+    glClearColor(0, 0, 0, 1);
+    
+    GLuint sp = createShaderProgram();
+
+    GLuint pointsVBO = 0;
+    glGenBuffers(1, &pointsVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, pointsVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+
+    //GLint vertexResolution = glGetUniformLocation(sp, "resolution");
+    //glUniform2f(vertexResolution, 640.0, 480.0);
+
+    GLint success;
+    GLchar inf[500];
+
+
+    GLuint vao = 0;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, pointsVBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, nullptr);
+    
+    GLfloat timerStart;
+    GLfloat timerEnd;
+
+    GLint timeLocation = glGetUniformLocation(sp, "time");
+
+    bool firstIt = 1;
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        if (firstIt) {
+            timerStart = clock();
+            firstIt = 0;
+        }
+        timerEnd = clock();
+
+        glUniform1f(timeLocation, (timerEnd - timerStart) / 1000.0);
+        glUseProgram(sp);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 4);    
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+
+    
+    }
+    
+    glfwTerminate();
+    return 0;
+}
---
+++ fragmentShader.glsl
@@ -0,0 +1,39 @@
+﻿#version 460
+
+out vec4 color;
+uniform float time;
+
+const float MAX_ITER = 128.0;
+
+float mandelbrot(vec2 uv) {
+	vec2 c = 4 * uv - vec2(0.7, 0.0);
+	c = c / pow(time, 4.0) - vec2(0.65, 0.45);
+	vec2 z = vec2(0.0);
+	float iter = 0.0;
+	for (float i; i < MAX_ITER; i++) {
+		z = vec2(z.x * z.x - z.y * z.y,
+				 2.0 * z.x * z.y) + c;
+	    if (dot(z, z) >	4.0) return iter / MAX_ITER;
+		iter++;
+	}
+	return 0.0;
+}
+
+vec3 hash13(float m) {
+	float x = fract(sin(m) * 5565.746);
+	float y = fract(sin(m + x) * 2219.486);
+	float z = fract(sin(x + y) * 4576.3);
+	return vec3(x, y, z);
+}
+
+void main() {
+	vec2 resolution = vec2(1080, 1080);
+	vec2 uv = (gl_FragCoord.xy - 0.5 * resolution.xy) / resolution.y;
+	vec3 col = vec3(0.0);
+
+	float m = mandelbrot(uv);
+	col += m;
+	//col += hash13(m);
+	col = pow(col, vec3(0.25));
+	color = vec4(col, 0.0);
+}
